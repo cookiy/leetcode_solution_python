0f7616904e449214790a91e81b35268a5188a622
wangguanjia <wangguanjia@zhuanzhuan.com>
2020-09-19 20:07:03 +0800

    提交题目

--- easy/404-sumOfLeftLeaves.py
+++ easy/404-sumOfLeftLeaves.py
@@ -35,3 +35,17 @@ class Solution:
             if r.right:
                 ans.append(r.right)
         return sum
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+      self.res = 0
+      def dfs(node):
+          if not node:
+              return None
+          if node.left and not node.left.left and not node.left.right:
+              self.res += node.left.val
+          dfs(node.left)
+          dfs(node.right)
+      dfs(root)
+      return self.res
