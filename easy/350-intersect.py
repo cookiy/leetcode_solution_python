9f84f5c1ede1692d38b9f57718dc1edb2162f3e9
cookiy wang <cookiy8511@gmail.com>
2020-10-18 10:55:12 +0800

    提交题目

--- easy/350-intersect.py
+++ easy/350-intersect.py
@@ -50,12 +50,22 @@ class Solution:
             m[num] += 1
         intersection = list()
         for num in nums2:
-            if(count := m.get(num, 0 )) > 0:
+            if(count := m.get(num, 0)) > 0:
                 intersection.append(num)
                 m[num] -= 1
                 if m[num] == 0:
                     m.pop(num)
         return intersection
+
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num1 = collections.Counter(nums1)
+        num2 = collections.Counter(nums2)
+        num = num1 & num2
+        return num.elements()
+
 
 
 """
