88a601fef8d32699182098042b9b11be3e7e0af0
cookiy wang <cookiy8511@gmail.com>
2020-10-23 19:56:50 +0800

    提交题目

---
+++ easy/387-firstUniqChar.py
@@ -0,0 +1,28 @@
+"""
+387. 字符串中的第一个唯一字符
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+ 
+
+示例：
+
+s = "leetcode"
+返回 0
+
+s = "loveleetcode"
+返回 2
+ 
+
+提示：你可以假定该字符串只包含小写字母。
+
+"""
+
+# hashtable
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
